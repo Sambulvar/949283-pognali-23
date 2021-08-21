bc5e232afb6c138304546305ef5d386bdf885c65
Артём <sambulvar@yandex.ru>
2021-08-21 11:36:49 +0300

    Раскладка начало

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,20 @@
-let siteList = document.querySelector('.site-list');
-let buttonSiteList = document.querySelector('.main-nav__toggle');
+let mainNav = document.querySelector('.main-nav');
 let pageHeader = document.querySelector('.page-header');
-buttonSiteList.addEventListener('click',function(){
-  if (siteList.classList.contains('visually-hidden')) {
-    siteList.classList.remove('visually-hidden');
-    logo.classList.remove('page-header--site-list');
+let logo = document.querySelector('.logo');
+let toggle = document.querySelector('.page-header__toggle')
+let backgroundTraveller = document.querySelector('.background-traveller');
+toggle.addEventListener('click',function(){
+  if (mainNav.classList.contains('visually-hidden')) {
+    mainNav.classList.remove('visually-hidden');
+    pageHeader.classList.add('page-header--nav-active');
+    logo.classList.add('logo--nav-active');
+    toggle.classList.add('page-header__toggle--nav-active');
+    backgroundTraveller.classList.add('background-traveller--nav-active');
   } else {
-    siteList.classList.add('visually-hidden');
-    pageHeader.classList.add('page-header--site-list');
+    mainNav.classList.add('visually-hidden');
+    pageHeader.classList.remove('page-header--nav-active');
+    logo.classList.remove('logo--nav-active');
+    toggle.classList.remove('page-header__toggle--nav-active');
+    backgroundTraveller.classList.remove('background-traveller--nav-active');
   }
 });
